9cc6e745b97bc5542d98724c1c8ee79b5d615855
Victor Costan <costan@gmail.com>
2009-01-18 19:50:55 -0500

    Blacklisted X_REAL_IP added by nginx.

--- app/controllers/web_support_controller.rb
+++ app/controllers/web_support_controller.rb
@@ -2,11 +2,13 @@ class WebSupportController < ApplicationController
   protect_from_forgery :except => [:echo]
   
   def echo
-    forbidden_hdrs = ['server_software'];
+    forbidden_hdrs = ['X_REAL_IP'];
     headers = request.headers.to_a.map { |hdr|
-         [hdr.first.to_s, hdr.last] }.select { |hdr| /^HTTP/ =~ hdr.first }
+         [hdr.first.to_s, hdr.last] }.select { |hdr|
+         /^HTTP/ =~ hdr.first }.reject { |hdr|
+         forbidden_hdrs.include? hdr.first }
     @headers = headers.map { |hdr| "#{hdr.first[5..-1]}: #{hdr.last}\n" }.
-                       sort.join        
+                       sort.join
          
     respond_to do |format|
       format.html # 

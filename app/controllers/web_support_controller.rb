97b300f5a7f4e9be085123fa33f7b73d3bc20b09
Victor Costan <costan@gmail.com>
2012-12-10 02:57:33 -0500

    Blacklist more Heroku-specific headers.

--- app/controllers/web_support_controller.rb
+++ app/controllers/web_support_controller.rb
@@ -4,15 +4,19 @@ class WebSupportController < ApplicationController
   protect_from_forgery :except => [:csv, :echo]
 
   def echo
-    forbidden_hdrs = ['X_REAL_IP', 'X_FORWARDED_FOR', 'X_HEROKU_CAN_EDIT',
-                      'X_HEROKU_USER', 'X_VARNISH'];
+    forbidden_hdrs = ['X_REAL_IP', 'X_FORWARDED_FOR', 'X_FORWARDED_PROTO',
+                      'X_HEROKU_DYNOS_IN_USE', 'X_HEROKU_QUEUE_DEPTH',
+                      'X_HEROKU_QUEUE_WAIT_TIME',
+                      'X_HEROKU_USER', 'X_REQUEST_START', 'X_VARNISH'];
     headers = request.headers.to_a.map { |hdr|
-         [hdr.first.to_s, hdr.last] }.select { |hdr|
-         /^HTTP/ =~ hdr.first }.reject { |hdr|
-         forbidden_hdrs.include? hdr.first[5..-1] }
+      [hdr.first.to_s, hdr.last]
+    }.select { |hdr|
+      /^HTTP/ =~ hdr.first
+    }.reject { |hdr|
+      forbidden_hdrs.include? hdr.first[5..-1]
+    }
     @uri = url_for
-    @headers = headers.map { |hdr| "#{hdr.first[5..-1]}: #{hdr.last}\n" }.
-                       sort.join
+    @headers = headers.map { |hdr| "#{hdr.first[5..-1]}: #{hdr.last}\n" }.sort
     @method = request.method.to_s
     @body = request.body.string
     respond_to do |format|

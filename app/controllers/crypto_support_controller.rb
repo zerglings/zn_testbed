9385ed77fb774b193263d98d98a30356eace739d
Victor Costan <costan@gmail.com>
2012-12-10 02:48:15 -0500

    Fixed a couple of Rails 2.3 errors.

--- app/controllers/crypto_support_controller.rb
+++ app/controllers/crypto_support_controller.rb
@@ -1,38 +1,35 @@
 class CryptoSupportController < ApplicationController
   protect_from_forgery :except => [:app_fprint, :device_fprint]
-  
-  # Don't log the (huge) binaries.
-  filter_parameter_logging :manifest
-  
+
   def device_fprint
     attrs = params[:attributes] || {}
-    
+
     # Reference code for how the digests are computed.
     @data = Imobile::CryptoSupportAppFprint.device_fprint_data attrs
     @hex_md5 = Imobile::CryptoSupportAppFprint.hex_device_fprint attrs
-    
+
     # In development mode, save the finger-print for test case generation.
     if RAILS_ENV == 'development'
       File.open('/tmp/device_fprint', 'w') do |f|
         f.write attrs.inspect + "\n" + @hex_md5 + "\n"
       end
     end
-    
+
     respond_to do |format|
       format.html # device_fprint.html.erb
       format.xml  # device_fprint.xml.builder
     end
   end
-  
+
   def app_fprint
     attrs = params[:attributes] || {}
     manifest = params[:manifest] || ' '
-    manifest = manifest.read unless manifest.respond_to? :to_str 
-    
+    manifest = manifest.read unless manifest.respond_to? :to_str
+
     # UTF-8 --> ASCII
     # TODO(costan): should Rails have done this for us?
     manifest = manifest.unpack('U*').pack('C*')
-    
+
     @hex_fprint =
         Imobile::CryptoSupportAppFprint.app_fprint_from_raw_data attrs, manifest
 
--- app/controllers/imobile_support_controller.rb
+++ app/controllers/imobile_support_controller.rb
@@ -1,24 +1,21 @@
 class ImobileSupportController < ApplicationController
   protect_from_forgery :except => [:payment_receipt, :push_notification]
 
-  # Don't log the push certificates. They're ugly and it's not cool.
-  filter_parameter_logging :certificate
-
   def payment_receipt
     production = params[:production] || 'false'
     production = production.read unless production.respond_to? :to_str
     production = (production == 'true')
-    
+
     receipt = params[:receipt] || ''
-    receipt = receipt.read unless receipt.respond_to? :to_str 
-    
+    receipt = receipt.read unless receipt.respond_to? :to_str
+
     # UTF-8 --> ASCII
     # TODO(costan): should Rails have done this for us?
-    receipt = receipt.unpack('U*').pack('C*')    
+    receipt = receipt.unpack('U*').pack('C*')
     if RAILS_ENV == 'development'
       File.open('/tmp/receipt', 'w') { |f| f.write receipt }
     end
-    
+
     server_type = production ? :production : :sandbox
     if params[:receipt].blank?
       @receipt = false
@@ -31,22 +28,22 @@ class ImobileSupportController < ApplicationController
       format.json { render :json => { :response => @receipt } }
     end
   end
-  
+
   def push_notification
-    certificate_blob = params[:certificate]    
-    
+    certificate_blob = params[:certificate]
+
     notification = params[:notification]
     device_info = params[:device]
     if certificate_blob and device_info
       # UTF-8 --> ASCII
       # TODO(costan): should Rails have done this for us?
-      certificate_blob = certificate_blob.unpack('U*').pack('C*') 
-      
+      certificate_blob = certificate_blob.unpack('U*').pack('C*')
+
       push_token = Imobile.pack_hex_push_token device_info[:app_push_token]
       notification[:push_token] = push_token
       Imobile.push_notification notification, certificate_blob
     end
-    
+
     respond_to do |format|
       format.html # push_notification.html.erb
       format.json { render :json => { :response => { :status => :ok }}}
